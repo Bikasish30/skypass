d47cc2b9504aa8352abc2b82d6238a1ceef02986
bikasish.b <bikasih.b@immidart.com>
2016-10-17 15:06:05 +0530

    Added comment

--- com.immidart.skypassTravel/src/test/java/com/immidart/skypassTravel/ManualRequestTest.java
+++ com.immidart.skypassTravel/src/test/java/com/immidart/skypassTravel/ManualRequestTest.java
@@ -13,6 +13,8 @@ import com.immidart.skypassTravel.businessLibrary.LogoutFromApplicationLib;
 import com.immidart.skypassTravel.businessLibrary.OperationTrackerSearchLib;
 import com.immidart.skypassTravel.genericLibrary.Driver;
 
+ // Create manual test
+
 @Listeners(com.immidart.skypassTravel.genericLibrary.Listener.class)
 public class ManualRequestTest {
 
